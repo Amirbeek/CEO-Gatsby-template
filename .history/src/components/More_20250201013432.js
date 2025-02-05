6268547e041a62fd759e15f49e070619759422d1
amirbeek <Amirbek.shomurodov01@gmail.com>
2025-02-05 22:19:17 +0000

    updated

--- .history/src/components/More_20250201013432.js
+++
@@ -1,7 +0,0 @@
-import * as React from "react"
-
-export default function More(){
-    return (<>
-        <h1>Amirbek Shomurodov</h1>
-    </>)
-}
--- .history/src/components/Posts_20250201155417.js
+++
@@ -1,9 +0,0 @@
-import { Card } from "@mui/material";
-
-export default function Posts(){
-    return(
-        <Card>
-            
-        </Card>
-    )
-}
